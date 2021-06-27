bcbac3758120b191748737e4ca1ca2560fc50c6b
rknaveen018 <83909786+rknaveen018@users.noreply.github.com>
2021-06-27 12:19:04 +0530

    Add files via upload

---
+++ number_guess_game.py
@@ -0,0 +1,12 @@
+import random
+guess = 0
+while guess < 4:
+    guess += 1
+    rand_num = random.randrange(10)
+    guessed_num = int(input("Enter the chosen number : "))
+    if guessed_num != rand_num:
+        print("You have " + str(4-guess) + " left in your hand")
+    else:
+        print("\n🎉🎉🎉 you got me 🎉🎉🎉")
+        break
+print("\n\t 👏👏👏 We had a great play 👏👏👏")
