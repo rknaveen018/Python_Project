93b46dc49209e076001a3077e18391850c7dd30e
Naveenkumar R K <rknaveen018@gmail.com>
2022-11-28 21:23:26 +0530

    Create the product array by multiplying other values except own value

---
+++ ProdArr.py
@@ -0,0 +1,14 @@
+l = [1, 2, 3, 4, -1, 23]
+
+l_product = []
+
+for i in range(len(l)):
+    val = 1
+    for j in range(len(l)):
+        if j != i:
+            val *= l[j]
+        else:
+            pass
+    l_product.append(val)
+
+print(l_product)
